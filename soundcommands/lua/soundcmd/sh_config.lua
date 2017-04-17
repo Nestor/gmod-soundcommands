c918acbda6bece1ebac2bb5c00688fb1c48019c3
Nicolas Str****** <nicolas.streng@gmail.com>
2017-04-17 22:00:37 +0200

    Removed unused config
    now this is very light

--- soundcommands/lua/soundcmd/sh_config.lua
+++ soundcommands/lua/soundcmd/sh_config.lua
@@ -2,7 +2,6 @@ soundcmd.config = {}
 soundcmd.config.distance = 75 -- default is 75
 soundcmd.config.pitch = 100 -- default is 100
 soundcmd.config.volume = 1 -- default is 1: from 0 to 1
-soundcmd.config.soundchannel = 1 -- default is 1: from 0 to 1
 
 soundcmd.config.onlyteam = false -- make true if you want only one team to have the rights to do that
 soundcmd.config.team = TEAM_CITIZEN
